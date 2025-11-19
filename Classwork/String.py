bbb6c605081736da4f932afb4f165c5c8c11eb56
Your Name <erusumallaleelabhavani@gmail.com>
2025-11-19 10:41:46 +0530

    String

---
+++ Classwork/String.py
@@ -0,0 +1,106 @@
+s=''
+s=""
+s=''' '''
+s="' '"
+name='Ajay'
+name1="Krishna"
+name + name1
+name+ ' '+ name1
+'Ajay Krishna'
+name
+name * 10
+'*'*100
+'-'*10
+name='Ajay Kumar'
+name[3]
+name[-2]
+name[-5]
+name[1]
+names='AjayKrishnaSatishNishithaPreethiRuchitha'
+names
+names[0:4]
+names[:4]
+names[4:11]
+names[11:17]
+names[17:25]
+names[25:32]
+names[32:40]
+names[32:]
+names[0:4:2]
+names[::3]
+names[-8:]
+names
+names[-15:-8]
+names[-23:-15]
+names[-29:-23]
+names[-36:-29]
+names[-40:-36]
+names[::-1]
+names
+names[4::-1]
+names[3::-1]
+names[10:4]
+names[10:4:-1]
+names[10:3:-1]
+names[16:10:-1]
+names[24:16:-1]
+names[31:24:-1]
+names[39:31:-1]
+names
+'AjayKrishnaSatishNishithaPreethiRuchitha'
+'Ajay' in names
+'nithin' in names
+'priya' in names
+'sahithi' not in names
+True
+names
+'AjayKrishnaSatishNishithaPreethiRuchitha'
+names.upper()
+names.lower()
+names.swapcase()
+l='python programming language'
+l.capitalize()
+l.title()
+"ß".casefold()
+'priya' in names
+
+l
+'python programming language'
+l.center(100,'*')
+l.center(50,'*')
+l.center(50,'-')
+l.center(50,'_')
+l.ljust(50,'-')
+l.ljust(50,' ')+':'
+l.rjust(50,'-')
+'2'.zfill(6)
+'202'.zfill(6)
+'202123'.zfill(6)
+names
+'AjayKrishnaSatishNishithaPreethiRuchitha'
+names.find('j')
+names.find('a')
+names.find('Ajay')
+names.find('z')
+names.rfind('a')
+names.index('K')
+names.index('a')
+names.rindex('a')
+names
+'AjayKrishnaSatishNishithaPreethiRuchitha'
+names.count('a')
+names.count('e')
+names.count('i')
+names
+'AjayKrishnaSatishNishithaPreethiRuchitha'
+names.replace('a','*')
+names.replace('sh','')
+names.replace('sh','00')
+names.replace('sh','-00-')
+names.replace('Ajay','Anirudh')
+names
+'AjayKrishnaSatishNishithaPreethiRuchitha'
+names.maketrans('aeiou','*****')
+names.translate(names.maketrans('aeiou','*****'))
+names.translate(names.maketrans('AEIOUaeiou','1234500000'))
+names.translate(str.maketrans('AEIOUaeiou','1234500000'))
