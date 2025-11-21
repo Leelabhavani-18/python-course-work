fea77e900692f294a82389169a03b624846d7631
Your Name <erusumallaleelabhavani@gmail.com>
2025-11-21 09:20:55 +0530

    Ifelse

---
+++ Classwork/Ifelse.py
@@ -0,0 +1,30 @@
+budget=int(input("Enter the budget: "))
+if budget>=20000:
+    print("Trip to Goa")
+    
+elif 15000<=budget<20000:
+    print("Goa for shopping")
+
+elif 10000<=budget< 15000:
+    print("clubbing")
+
+elif 5000<=budget< 10000:
+    print("Go for cafe")
+
+elif 2000<=budget< 5000:
+    print("Go for movie")
+
+elif 1000<=budget< 2000:
+    print("Go for temple")
+
+elif 500<=budget< 1000:
+    print("Go for nails")
+
+elif 100<=budget< 500:
+    print("Mobile Recharge")
+
+elif 10<=budget< 100:
+    print("Having tea")
+
+elif 5<=budget< 50:
+    print("Go to sleep")
