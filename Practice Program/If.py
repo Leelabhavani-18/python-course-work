ee105593231da57fe92bc8aec6c5c1c411064462
Your Name <erusumallaleelabhavani@gmail.com>
2025-11-21 09:15:55 +0530

    If

---
+++ Practice Program/If.py
@@ -0,0 +1,21 @@
+send=False
+if send:
+   print("Send the message")
+
+
+send=True
+if send:
+   print("Send the message")
+
+
+data={'leela@gmail.com':1234,
+      'chintu@gmail,com':5678,
+      'thriveni@gmail.com':9012
+      }
+mail=input("Enter the mail: ")
+pwd=int(input("Enter the Password: "))
+
+if data.get(mail)==pwd:
+    print("Valid Login")
+else:
+    print("Invalid Login")
